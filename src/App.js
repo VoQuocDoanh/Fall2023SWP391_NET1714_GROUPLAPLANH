b0146d1d85d5ac132057ada254a29a0b160abd7b
Minh Hiển <hiennmse170279@fpt.edu.vn>
2023-09-27 19:26:59 +0700

    Categories folder

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CreateBeat from './beat/CreateBeat';
 import ListUsers from './user/ListUsers';
 import UpdateBeat from './beat/UpdateBeat';
 import './App.css'
+import Test2 from './components/Test2';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route exact path='/CreateBeat' element={<CreateBeat/>}/>
           <Route exact path='/ListUsers' element={<ListUsers/>}/>
           <Route exact path='/UpdateBeat/:id' element={<UpdateBeat/>}/>
+          <Route exact path='/Test2' element={<Test2/>}/>
         </Routes>
       </Router>          
     </div>
