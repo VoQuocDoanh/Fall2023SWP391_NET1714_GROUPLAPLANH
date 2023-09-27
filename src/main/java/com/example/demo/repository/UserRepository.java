de3b03c2ccf1c199de6267b20125327a8416009a
Minh Khang <khangminh530@gmail.com>
2023-09-27 11:04:58 +0700

    rac'

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,29 +1,22 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByUsernameAndPass (String username, String pass);
-    User findUserByUsername(String username);
-
-//    Optional<User>findByUsername(String username);
-//    User findByUsername(String username);
-   // org.apache.el.stream.Optional findById();
+    Optional<User> findUserByUsername(String username, String pass);
 
     List<User> findByOrderByStatusDesc();
 
     @Query("SELECT u FROM User u WHERE u.username like %:username%")
     List<User> searchByUserName(String username);
-    @Query("SELECT u FROM User u WHERE u.username = :userName")
-    User findByUsername(String userName);
 
+    Optional<User> findUserByUsername(String username);
 }
