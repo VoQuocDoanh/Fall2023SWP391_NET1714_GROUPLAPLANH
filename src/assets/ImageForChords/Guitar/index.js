3ad17c7910fe521b69875fa04b30ec686c2c3c1f
VoQuocDoanh <doanhvqse170195@fpt.edu.vn>
2023-10-15 22:53:34 +0700

    chordsdetails

---
+++ src/assets/ImageForChords/Guitar/index.js
@@ -0,0 +1,142 @@
+const GUITAR = [
+    {
+        type: "A7",
+        key: "A",
+        suffix: "7",
+        img: require("../Guitar/A/A7.png"),
+    },
+    {
+        type: "Am7",
+        key: "A",
+        suffix: "m7",
+        img: require("../Guitar/A/Am7.png"),
+    },
+    {
+        type: "Amaj7",
+        key: "A",
+        suffix: "maj7",
+        img: require("../Guitar/A/Amaj7.png")
+    },
+    {
+        type: "Amajor",
+        key: "A",
+        suffix: "major",
+        img: require("../Guitar/A/Amajor.png")
+    },
+    {
+        type: "Aminor",
+        key: "A",
+        suffix: "minor",
+        img: require("../Guitar/A/Aminor.png"),
+    },
+    {
+        type: "B7",
+        key: "B",
+        suffix: "7",
+        img: require("../Guitar/B/B7.png"),
+    },
+    {
+        type: "Bm7",
+        key: "B",
+        suffix: "m7",
+        img: require("../Guitar/B/Bm7.png"),
+    }, {
+        type: "Bmaj7",
+        key: "B",
+        suffix: "maj7",
+        img: require("../Guitar/B/Bmaj7.png"),
+    }, {
+        type: "Bmajor",
+        key: "B",
+        suffix: "maj7",
+        img: require("../Guitar/B/Bmajor.png")
+    }, {
+        type: "Bminor",
+        key: "B",
+        suffix: "minor",
+        img: require("../Guitar/B/Bminor.png"),
+    }, {
+        type: "C7",
+        key: "C",
+        suffix: "7",
+        img: require("../Guitar/C/C7.png"),
+    }, {
+        type: "Cm7",
+        key: "C",
+        suffix: "m7",
+        img: require("../Guitar/C/Cm7.png"),
+    },  {
+        type: "Cmaj7",
+        key: "C",
+        suffix: "maj7",
+        img: require("../Guitar/C/Cmaj7.png"),
+    },{
+        type: "Cmajor",
+        key: "C",
+        suffix: "major",
+        img: require("../Guitar/C/Cmajor.png"),
+    }, {
+        type: "Cminor",
+        key: "C",
+        suffix: "minor",
+        img: require("../Guitar/C/Cminor.png"),
+    },{
+        type: "Dmaj7",
+        key: "D",
+        suffix: "maj7",
+        img: require("../Guitar/D/Dmaj7.png"),
+    }, {
+        type: "Dmajor",
+        key: "D",
+        suffix: "major",
+        img: require("../Guitar/D/Dmajor.png"),
+    }, {
+        type: "Dminor",
+        key: "D",
+        suffix: "minor",
+        img: require("../Guitar/D/Dminor.png"),
+    }, {
+        type: "D7",
+        key: "D",
+        suffix: "7",
+        img: require("../Guitar/D/D7.png"),
+    },
+    {
+        type: "Dm7",
+        key: "D",
+        suffix: "m7",
+        img: require("../Guitar/D/Dm7.png"),
+    },
+    {
+        type: "E7",
+        key: "E",
+        suffix: "7",
+        img: require("../Guitar/E/E7.png"),
+    },
+    {
+        type: "Em7",
+        key: "E",
+        suffix: "m7",
+        img: require("../Guitar/E/Em7.png"),
+    },
+    {
+        type: "Emaj7",
+        key: "E",
+        suffix: "maj7",
+        img: require("../Guitar/E/Emaj7.png")
+    },
+    {
+        type: "Emajor",
+        key: "E",
+        suffix: "major",
+        img: require("../Guitar/E/Emajor.png")
+    },
+    {
+        type: "Eminor",
+        key: "E",
+        suffix: "minor",
+        img: require("../Guitar/E/Eminor.png"),
+    },
+]
+
+export default GUITAR;
